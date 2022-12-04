ed08488a5acc0f58547295b4e0525e4e3d94f0f9
kimsky <developer.kimsky@gmail.com>
2022-12-04 22:47:38 +0900

    add card and edit form component

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./editor.module.css";
+import CardEditForm from "../card_edit_form/card_edit_form";
 
-const Editor = (props) => (
-  <section className={styles.editor}>
-    <h1 className={styles.title}>Card Maker</h1>
-  </section>
-);
+const Editor = ({ cards }) => {
+  return (
+    <section className={styles.editor}>
+      <h1 className={styles.title}>Card Maker</h1>
+      <ul>
+        {cards.map((card) => (
+          <CardEditForm card={card} />
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default Editor;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./maker.module.css";
 import Header from "../header/header";
 import Footer from "../footer/footer";
@@ -7,6 +7,41 @@ import Preview from "../preview/preview";
 import { useNavigate } from "react-router-dom";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "Kimsky",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "developer.kimsky@gmail.com",
+      message: "go for it",
+      fileName: "file",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "Kimsky2",
+      company: "Samsung",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "developer.kimsky@gmail.com",
+      message: "go for it",
+      fileName: "file",
+      fileURL: null,
+    },
+    {
+      id: "3",
+      name: "Kimsky3",
+      company: "Samsung",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "developer.kimsky@gmail.com",
+      message: "go for it",
+      fileName: "file",
+      fileURL: null,
+    },
+  ]);
   const navigate = useNavigate();
 
   const onLogout = () => {
@@ -25,8 +60,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
