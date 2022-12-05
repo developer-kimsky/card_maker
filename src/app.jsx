b0d7d5b395a10c98fff6bd94375c351aa8d9987b
kimsky <developer.kimsky@gmail.com>
2022-12-05 18:31:25 +0900

    handle image uploading

--- src/app.jsx
+++ src/app.jsx
@@ -3,13 +3,16 @@ import Login from "./components/login/login";
 import Maker from "./components/maker/maker";
 import styles from "./app.module.css";
 
-function App({ authService }) {
+function App({ FileInput, authService }) {
   return (
     <div className={styles.app}>
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Login authService={authService} />} />
-          <Route path="/maker" element={<Maker authService={authService} />} />
+          <Route
+            path="/maker"
+            element={<Maker FileInput={FileInput} authService={authService} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from "./card_edit_form.module.css";
 import Button from "../button/button";
-import ImageFileInput from "../image_file_input/image_file_input";
 
-const CardEditForm = ({ card, updateCard, deleteCard }) => {
+const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
   const { name, company, title, email, message, theme, fileName, fileURL } =
     card;
 
   const onSubmit = () => {
     deleteCard(card);
+  };
+
+  const onFileChange = (file) => {
+    console.log(file);
+    updateCard({
+      ...card,
+      fileName: file.name,
+      fileURL: file.url,
+    });
   };
 
   const onChange = (event) => {
@@ -69,7 +77,7 @@ const CardEditForm = ({ card, updateCard, deleteCard }) => {
         onChange={onChange}
       />
       <div className={styles.fileInput}>
-        <ImageFileInput />
+        <FileInput name={fileName} onFileChange={onFileChange} />
       </div>
       <Button name="Delete" onClick={onSubmit} />
     </form>
